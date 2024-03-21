55be095b68930e493b2786a4ecb5b65fd0becdf5
Christopher-VA <cvonaltenstadt@gmail.com>
2024-03-21 20:43:15 +0800

    Adding Querys

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,24 +1,30 @@
-const { User, Thought } = require('../models');
+const { User, Post } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
     users: async () => {
-      return User.find().populate('thoughts');
+      return User.find().populate('posts');
     },
     user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
+      return User.findOne({ username }).populate('posts');
     },
-    thoughts: async (parent, { username }) => {
+    posts: async (parent, { username }) => {
       const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
+      return Post.find(params).sort({ createdAt: -1 });
     },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
+    post: async (parent, { postId }) => {
+      return Post.findOne({ _id: postId });
+    },
+    genres: async () => {
+      return Genre.find().populate('posts');
+    },
+    genre: async () => {
+      return Genre.findOne({ genreName }).populate('posts');
     },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
+        return User.findOne({ _id: context.user._id }).populate('posts');
       }
       throw AuthenticationError;
     },
@@ -46,72 +52,6 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, user };
-    },
-    addThought: async (parent, { thoughtText }, context) => {
-      if (context.user) {
-        const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw AuthenticationError;
-    },
-    addComment: async (parent, { thoughtId, commentText }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $addToSet: {
-              comments: { commentText, commentAuthor: context.user.username },
-            },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-      }
-      throw AuthenticationError;
-    },
-    removeThought: async (parent, { thoughtId }, context) => {
-      if (context.user) {
-        const thought = await Thought.findOneAndDelete({
-          _id: thoughtId,
-          thoughtAuthor: context.user.username,
-        });
-
-        await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { thoughts: thought._id } }
-        );
-
-        return thought;
-      }
-      throw AuthenticationError;
-    },
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
-              },
-            },
-          },
-          { new: true }
-        );
-      }
-      throw AuthenticationError;
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,27 +4,34 @@ const typeDefs = `
     username: String
     email: String
     password: String
-    create_at: Date
+    posts: [Post]
+    createAt: Date
   }
 
   type Post {
     _id: ID
     title: String
     body: String
-    users: [User]
-    genres: [Genre]
+    author: String
+    genre: String
+    createAt
   }
 
   type Genre {
     _id: ID
-    name: String
+    genreName: String
     posts: [Post]
   }
 
   type Query {
     users: [User]
+    user(username: String!): User
     genres: [Genre]
+    genre(name: String!): Genre
     posts: [Post]
+    post(author: String!); Post
+    postsInGenre(genre: String!): Integer
+    me: User
   }
 
   type Mutation {
